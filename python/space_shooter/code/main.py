400417a6c368dc976a52e73fa82dc67e065e11f1
NahianMaksood04 <nahianmaksood04@gmail.com>
2026-02-23 20:03:44 +0600

    23-02-2026 | 20:03:44 - extra

--- python/space_shooter/code/main.py
+++ python/space_shooter/code/main.py
@@ -1,13 +1,32 @@
 import pygame
+import random
 
 pygame.init()
-height, width = 600,400
-display_surface = pygame.display.set_mode((height,width))
+
+w, h = 1000, 500
+dis_surf = pygame.display.set_mode((w, h))
+pygame.display.set_caption("space_shooter")
 running = True
+
+
+plane = pygame.image.load("../images/player.png").convert_alpha()
+star = pygame.image.load("../images/star.png").convert_alpha()
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    dis_surf.fill("black")
+
+    dis_surf.blit(plane, (100, 400))
+
+    random_x = random.randint(950, 450)
+    random_y = random.randint(950, 450)
+
+    dis_surf.blit(star, (random_x, random_y))
+
+    pygame.display.update()
 
 pygame.quit()
